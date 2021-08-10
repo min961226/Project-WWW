3d0ffeed34581ed2f57fb9a2fdcf92bd46b897ef
deoksu Jo <antisnoopy88@gmail.com>
2021-08-10 17:39:34 +0900

    20210810_jodeoksu_2

--- src/com/qs/www/mypage/controller/UpdateMypageServlet.java
+++ src/com/qs/www/mypage/controller/UpdateMypageServlet.java
@@ -1,21 +1,26 @@
 package com.qs.www.mypage.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/mypage/info/update")
 public class UpdateMypageServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		request.getRequestDispatcher("/WEB-INF/views/mypage/info.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/views/mypage/updateInfo.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		HttpSession session = request.getSession();
+		
+		System.out.println("Session : " + session);
 	}
 }
