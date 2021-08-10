7b642a330b6504c57eac10fde3d381674efbb81d
yeseul JO <cysysc102@gmail.com>
2021-08-10 17:40:20 +0900

    근무신청시, TBL_ITEM_CONTENT에 insert 성공

--- src/com/qs/www/schedule/controller/InsertWorkingSystemScheduleServlet.java
+++ src/com/qs/www/schedule/controller/InsertWorkingSystemScheduleServlet.java
@@ -1,6 +1,9 @@
 package com.qs.www.schedule.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,8 +11,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.qs.www.member.model.dto.MemberDTO;
 import com.qs.www.schedule.model.dto.MemberWorkLogDTO;
 import com.qs.www.schedule.model.dto.ReportDTO;
+import com.qs.www.schedule.model.dto.WorkingDocumentItemDTO;
 import com.qs.www.schedule.model.service.ScheduleService;
 
 @WebServlet("/schedule/workingSystem/insert")
@@ -28,10 +33,14 @@ public class InsertWorkingSystemScheduleServlet extends HttpServlet {
 		
 		System.out.println("InsertWorkingSystemScheduleServlet = 근무신청 서블렛으로 이동");
 		
+//		HttpSession session = request.getSession();
+//		MemberDTO loginMember = (MemberDTO) session.getAttribute("loginMember");
+		
+		//근무신청서의 문서번호는 4번이다.
+		int documentNo = 4;
+		
 		int workNo = Integer.parseInt(request.getParameter("workNo"));
 		int approverLine = Integer.parseInt(request.getParameter("approverLine"));
-		java.sql.Date startDay = java.sql.Date.valueOf(request.getParameter("startDay"));
-		java.sql.Date endDay = java.sql.Date.valueOf(request.getParameter("endDay"));
 		String changeReason = request.getParameter("changeReason");
 		
 		String workType = "";
@@ -41,39 +50,96 @@ public class InsertWorkingSystemScheduleServlet extends HttpServlet {
 			workType = "표준근무제";
 		}
 		
-		System.out.println("workNo : " + workNo);
-		System.out.println("근무제타입 : " + workType);
-		System.out.println("결재라인 : " + approverLine);
-		System.out.println("시작일 : " + startDay);
-		System.out.println("종료일 : " + endDay);
-		System.out.println("변경사유 : " + changeReason);
+		/* 1. 상신테이블(TBL_REPORT)에 상신문서 insert */
+		ReportDTO reportDTO = new ReportDTO();
+		//reportDTO.setMemberNo(loginMember.getMemberNo());
+		reportDTO.setMemberNo(4);
+		reportDTO.setDocumentNo(documentNo);
+		reportDTO.setReportNote(changeReason);
+		System.out.println("InsertWorkingSystemScheduleServlet의 reportDTO : " + reportDTO);
+				
+		ScheduleService scheduleService = new ScheduleService();		
+//		int result = scheduleService.applyWorkingSystem(reportDTO);
+//		System.out.println(result);
+//		if(result > 0 ) { 여기서부터 다음거 시작이지
 		
-//		HttpSession session = request.getSession();
-//		int memberNo = (Integer) session.getAttribute("memberNo");
-//		System.out.println(memberNo);
+		
+		/* 2-1. 방금 상신올린문서의 ReportNo를 가져와야 함 */
+		//currval로 가져오면 되지않나?
+		
+		/* 2. 상신별문서항목작성내용(TBL_ITEM_CONTENT)에 insert*/
+		//String memberName = (String) session.getAttribute("name");
+		//String title = memberName + workType + " 신청서";
+		String title = "조예슬" + workType + " 신청서";
+		String getworkNo = request.getParameter("workNo");
+		
+		System.out.println("title : " + title);
+		
+		//ArrayList를 써야 하나?
+		List<String> workingDocumentItem = new ArrayList<>();
+		workingDocumentItem.add(title);
+		workingDocumentItem.add(getworkNo);
+		workingDocumentItem.add(request.getParameter("startDay"));
+		workingDocumentItem.add(request.getParameter("endDay"));
+		workingDocumentItem.add(changeReason);
+		System.out.println("InsertWorkingSystemScheduleServlet의 List : " + workingDocumentItem);
+		
+		int priority = 1;
+		int result = 0;
+		for(String item : workingDocumentItem) {
+			WorkingDocumentItemDTO workingDocumentItemDTO = new WorkingDocumentItemDTO();
+			workingDocumentItemDTO.setReportNo(2); //일단 하드코딩
+			workingDocumentItemDTO.setDocumentNo(documentNo);
+			workingDocumentItemDTO.setPriority(priority);
+			workingDocumentItemDTO.setItemContent(item);			
+			
+			result = scheduleService.applyWorkingSystemItemContent(workingDocumentItemDTO);
+			
+			priority++;
+		}
+		System.out.println(result);
+		
+//		workingDocumentItemDTO.setTitle(title);
+//		workingDocumentItemDTO.setWorkNo(getworkNo);
+//		workingDocumentItemDTO.setStartDate(request.getParameter("startDay"));
+//		workingDocumentItemDTO.setEndDate(request.getParameter("endDay"));
+//		workingDocumentItemDTO.setChangeReason(changeReason);
+//		System.out.println("InsertWorkingSystemScheduleServlet의 workingDocumentItemDTO : " + workingDocumentItemDTO);
+//		
+//		int result = scheduleService.applyWorkingSystemItemContent(workingDocumentItemDTO);
+//		System.out.println(result);
+//		if(result > 0 ) { 여기서부터 다음거 시작이지
+		
+		
+		/* 3-1. 결재라인선택한 번호로, 결재자들의 결재자사번을 받아오기 */
+		List<String> approverList = new ArrayList<>();
+		
+		
+		/* 3-2. 상신별결재자(TBL_APPROVER_PER_REPORT)에 insert */
+		java.sql.Date startDay = java.sql.Date.valueOf(request.getParameter("startDay"));
+		java.sql.Date endDay = java.sql.Date.valueOf(request.getParameter("endDay"));
+		//int memberNo = (Integer) session.getAttribute("memberNo");
+		//System.out.println("memberNo : " + memberNo);
 		
 		MemberWorkLogDTO memberWorkLogDTO = new MemberWorkLogDTO();
-		memberWorkLogDTO.setWorkNo(workNo);
+		memberWorkLogDTO.setMemberNo(4);
 		memberWorkLogDTO.setWorkType(workType);
+		memberWorkLogDTO.setWorkNo(workNo);
 		memberWorkLogDTO.setApproverLine(approverLine);
 		memberWorkLogDTO.setStartDay(startDay);
 		memberWorkLogDTO.setEndDay(endDay);
 		memberWorkLogDTO.setChangeReason(changeReason);
 		
-		//일단 하드코딩
-		ReportDTO reportDTO = new ReportDTO();
-		reportDTO.setMemberNo(4);
-		reportDTO.setDocumentNo(4);
-		reportDTO.setReportNote(changeReason);
-		System.out.println("InsertWorkingSystemScheduleServlet의 reportDTO : " + reportDTO);
-		
-		ScheduleService scheduleService = new ScheduleService();		
-		int result = scheduleService.applyWorkingSystem(reportDTO);
-		
-		System.out.println(result);
 		
 		
 		
+		if(result > 0) {
+			System.out.println("상신성공");
+			String path = "";
+			response.sendRedirect(request.getContextPath());
+		} else {
+			System.out.println("상신실패");
+		}
 		
 	}
 }
