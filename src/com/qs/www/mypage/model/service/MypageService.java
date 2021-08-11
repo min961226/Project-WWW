85b0208693048315b52807f97908884f3a6b3ef2
deoksu Jo <antisnoopy88@gmail.com>
2021-08-11 11:00:16 +0900

    20210811_jodeoksu_1

--- src/com/qs/www/mypage/model/service/MypageService.java
+++ src/com/qs/www/mypage/model/service/MypageService.java
@@ -1,11 +1,6 @@
 package com.qs.www.mypage.model.service;
 
-import static com.qs.www.common.mybatis.Template.getSqlSession;
-
-import org.apache.ibatis.session.SqlSession;
-
 import com.qs.www.mypage.model.dao.MypageDAO;
-import com.qs.www.mypage.model.dto.MemberInfoDTO;
 
 public class MypageService {
 	
@@ -13,16 +8,5 @@ public class MypageService {
 	
 	public MypageService() {
 		mypageDAO = new MypageDAO();
-	}
-
-	public MemberInfoDTO selectInfo(String memberId) {
-		
-		SqlSession session = getSqlSession();
-		
-		MemberInfoDTO memberInfo = mypageDAO.selectInfo(session, memberId);
-		
-		session.close();
-		
-		return memberInfo;
 	}
 }
