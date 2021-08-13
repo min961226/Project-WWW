9cdd048c27d3e28b58245f0e5b8eda7b35c67978
gggk105 <gggmggg105@gmail.com>
2021-08-14 01:47:09 +0900

    01:46 커밋

--- src/com/qs/www/board/controller/UpdateFreeBoardServlet.java
+++ src/com/qs/www/board/controller/UpdateFreeBoardServlet.java
@@ -15,22 +15,6 @@ public class UpdateFreeBoardServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		int no = Integer.parseInt(request.getParameter("no"));
-		
-		FreeService freeService = new FreeService();
-		FreeDTO free = freeService.selectFreeDetail(no);
-		
-		String path = "";
-		if(free != null) {
-			path = "/WEB-INF/views/board/updateFreeBoard.jsp";
-			request.setAttribute("free", free);
-		} else {
-			path = "/WEB-INF/views/common/failed.jsp";
-			request.setAttribute("message", "자유게시판 수정용 조회하기 실패!");
-		}
-		
-		request.getRequestDispatcher(path).forward(request, response);
-		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
